51eb258716a10af9419d4fc84a47729781569b3c
Yalin-M <Yalin@Yalin-PC>
2014-04-08 01:03:35 +0300

    More exception handling

---
+++ Bomberman/src/game/models/tests/BombExplosionTest.java
@@ -0,0 +1,34 @@
+package game.models.tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import game.models.Bomb;
+
+
+public class BombExplosionTest {
+	@BeforeClass
+	public static void setUpBeforeTest() throws Exception{
+	}
+	
+	@Test
+	public void test(){
+		
+		int delta = 200;
+		Bomb bomb = new Bomb(0, 0, 1, 700);
+		float expectedLoopCount = bomb.getDetonationTime() / delta;	
+		int i;
+		
+		for(i = 0; i < expectedLoopCount + 5; i++) {
+			if(bomb.isExploded(delta)){
+					System.out.println("Bomb exploded.");
+					break;
+			}		
+		}
+		if(i > expectedLoopCount)
+			fail("Bomb didn't explode.");
+	}
+}
+
